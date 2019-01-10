18721b1fc6ff7d99c0f43d63eb4c9839f747d460
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-10 09:24:10 -1000

    file setup

---
+++ ruh-roh.js
@@ -0,0 +1,23 @@
+'use strict';
+const dataset = require('./email_log.json');
+
+/**
+ * {
+ *   emails: [
+ *      {
+ *        "email": "jay@mail.net",
+ *        "sent": "Thu, 4 June 2015 23:04:49 GMT"
+ *      },
+ *    ]
+ *  }
+ */
+
+const logEmails = () => {
+  
+
+  return {
+
+  }
+}
+
+console.log(dataset);
